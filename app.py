3c83bfda3a5a03b41cd6b89528e9af5e0372919d
Sanny Raj <sannyrajstm0814@gmail.com>
2026-08-13 22:07:43 +0530

    Add employee search feature

--- app.py
+++ app.py
@@ -10,3 +10,22 @@ with open("employees.csv", "r") as file:
             employee["department"],
             employee["salary"]
         )
+
+def search_employee(employee_id):
+    with open("employees.csv", "r") as file:
+        employees = csv.DictReader(file)
+
+        for employee in employees:
+            if employee["id"] == employee_id:
+                print("\nEmployee Found")
+                print("ID:", employee["id"])
+                print("Name:", employee["name"])
+                print("Department:", employee["department"])
+                print("Salary:", employee["salary"])
+                return
+
+    print("\nEmployee not found")
+
+
+employee_id = input("Enter Employee ID: ")
+search_employee(employee_id)
